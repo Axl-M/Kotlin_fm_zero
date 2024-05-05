a00c2ca5bd7bec6a04c40b7f04fb4b78f21657f9
Axl-M <axlmalyasov@gmail.com>
2024-05-05 18:30:02 +1000

    lesson3

---
+++ src/lesson1_variables.kt
@@ -0,0 +1,23 @@
+fun main() {
+    println("Hello World!")
+
+    // числовые
+    val intNum1: Int = -2147483648 // 32 bit
+    val intNum2: Int = 2147483647 // 32 bit
+    val longNum: Long // 64 bit
+    val shrtNum: Short // 16 bit
+    val byteNum: Byte // 8 bit
+    val unsignedNum: UInt = 2147483647u // 32 bit (only positive)
+
+    //    вещественные
+    val doubleNun: Double = 234.435 // 64 bit
+    val floatNum: Float = 31.57f // 32 bit
+
+    // строковые
+    val stringValue: String = "Это строка"
+    val charValue: Char = '1'
+
+    // логические
+    val trueValue: Boolean = true
+
+}
---
+++ src/lesson2_operators.kt
@@ -0,0 +1,37 @@
+fun main() {
+    val a = 5
+    val b = 7
+    println(a + b)
+
+    val intNum1: Int = 10   // указание типа здесь излишне
+    val intNum2: Int = 3
+    println(intNum1 / intNum2) // 3 так как передали целые числа, дробна часть отбрасывается
+
+    val floatNum1: Float = 10f  // указание типа здесь излишне
+    val floatNum2: Float = 3f
+    println(floatNum1 / floatNum2)  // 3.3333333
+
+    val doubleNum1: Double = 10.0   // указание типа здесь излишне
+    val doubleNum2: Double = 3.0
+    println(doubleNum1 / doubleNum2)  // 3.3333333333333335
+
+    println(10 % 3)     // остаток от деления = 1
+
+    val c = floatNum1 + doubleNum1
+    println(c)
+    println(c::class)               // ссылка на Kotlin-class (double (Kotlin reflection is not available))
+    println(c::class.simpleName)    // имя класса  (Double)
+
+    var counter = 0
+    counter = counter + 1
+    counter += 1
+    counter++
+    println(counter)
+
+    counter -= 1
+    counter--
+
+    ++counter
+    --counter
+
+}
