23b0f393e6eb28a7f6c2fdeba00e98609f6d78ee
Axl-M <axlmalyasov@gmail.com>
2024-05-05 18:56:23 +1000

    lesson3 completed

--- src/lesson3_strings.kt
+++ src/lesson3_strings.kt
@@ -1,4 +1,22 @@
 fun main() {
     val greeting = "Здравстуйте"
     val userName = "мой повелитель"
+
+    println("$greeting, $userName! Какое будет указание?")
+    println("Я могу доставить для вас ${10 + 6 } девствениц.")
+
+    // многострочник
+    val multyString = """
+           строка1
+        строка2
+                строка3
+    """.trimIndent()        // удалит прбелы в начале строк выровняв по итнимальному отступу
+    println(multyString)
+
+    val multyString2 = """
+           |строка1
+        |строка2
+               | строка3
+    """.trimMargin()        // удалит прбелы в начале строк выровняв по |
+    println(multyString2)
 }
