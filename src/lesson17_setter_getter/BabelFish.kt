92b1b291bab85dba0e71ade7cdaf99c4a9396334
Axl-M <axlmalyasov@gmail.com>
2024-05-24 19:31:20 +1000

    lesson 16 getter i setter automatically created

---
+++ src/lesson17_setter_getter/BabelFish.kt
@@ -0,0 +1,6 @@
+package lesson17_setter_getter
+
+class BabelFish {
+    var nerveSignalLel = 200
+
+}
--- src/lesson17_setter_getter/lesson17.kt
+++ src/lesson17_setter_getter/lesson17.kt
@@ -1,2 +1,10 @@
 package lesson17_setter_getter
 
+fun main () {
+    val fish = BabelFish()
+
+    println("old value: ${fish.nerveSignalLel}")
+    fish.nerveSignalLel = 400
+    println("new value: ${fish.nerveSignalLel}")
+
+}
