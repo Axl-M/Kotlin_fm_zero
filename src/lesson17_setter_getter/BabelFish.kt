e934d0eedf4d9c67f56812ee3e193acec3f02e8d
Axl-M <axlmalyasov@gmail.com>
2024-05-24 20:25:40 +1000

    lesson 16 completed

--- src/lesson17_setter_getter/BabelFish.kt
+++ src/lesson17_setter_getter/BabelFish.kt
@@ -1,6 +1,25 @@
 package lesson17_setter_getter
 
-class BabelFish {
-    var nerveSignalLel = 200
+// getter i setter automatically created
+//class BabelFish {
+//    var nerveSignalLel = 200
+//}
 
+class BabelFish (
+    private val coefficient: Int?
+){
+    var isTranslated = false
+    var nerveSignalLel = 200
+        // можно прописать самостотельно что она будет отдавать (field -означает значение проинициализированное по-умолчанию)
+//        get() = field
+        get() = if (coefficient != null) field * coefficient else field  // обязательно здесь использовать field (имя переменной вызовет рекурсию)
+         // либо установить что она будет возвращать всегда
+//        get() = 250
+        set(value) {
+            field = value
+            if(value > 300){
+                isTranslated = true
+                println("isTranslated = true")
+            }
+        }
 }
--- src/lesson17_setter_getter/lesson17.kt
+++ src/lesson17_setter_getter/lesson17.kt
@@ -1,10 +1,18 @@
 package lesson17_setter_getter
 
 fun main () {
-    val fish = BabelFish()
+    val fish = BabelFish(null)
 
     println("old value: ${fish.nerveSignalLel}")
-    fish.nerveSignalLel = 400
-    println("new value: ${fish.nerveSignalLel}")
+//    println("old value: ${fish.isTranslated}")
+//    fish.nerveSignalLel = 400
+//    println("new value: ${fish.nerveSignalLel}")
+//    println("old value: ${fish.isTranslated}")
+
+    val fish2 = BabelFish(2)
+    println("old value: ${fish2.nerveSignalLel}")
+
+    val fish3 = BabelFish(21)
+    println("old value: ${fish3.nerveSignalLel}")
 
 }
