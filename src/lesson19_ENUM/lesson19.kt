44398d9a70625faf9a8ab407196b208836c0aa14
Axl-M <axlmalyasov@gmail.com>
2024-05-26 13:46:59 +1000

    lesson 19 ENUM 2

--- src/lesson19_ENUM/lesson19.kt
+++ src/lesson19_ENUM/lesson19.kt
@@ -12,23 +12,45 @@ fun main(){
         Thread.sleep(1000)
     }
 
+//    for (i in statusesFromServer){
+//        when(i){
+//            101 -> setStatus(Status.NEW)
+//            102 -> setStatus(Status.COOKING)
+//            103 -> setStatus(Status.COMPLETED)
+//            else -> setStatus(Status.ERROR)
+//        }
+//        Thread.sleep(1000)
+//    }
+
     for (i in statusesFromServer){
         when(i){
-            101 -> setStatus(Status.NEW)
-            102 -> setStatus(Status.COOKING)
-            103 -> setStatus(Status.COMPLETED)
+            Status.NEW.id -> setStatus(Status.NEW)
+            Status.COOKING.id -> setStatus(Status.COOKING)
+            Status.COMPLETED.id -> setStatus(Status.COMPLETED)
             else -> setStatus(Status.ERROR)
         }
-        Thread.sleep(1000)
+        Thread.sleep(500)
     }
 
+    println()
+    println(Status.NEW.name)    // имя задекларированной константы
+    println(Status.NEW.ordinal) // её порядковый номер
+    // values - массив ENUM-ов
+    for (i in Status.values()) println(i)
 }
 
-enum class Status {
-    NEW,
-    COOKING,
-    COMPLETED,
-    ERROR,
+//enum class Status {
+//    NEW,
+//    COOKING,
+//    COMPLETED,
+//    ERROR,
+//}
+
+enum class Status(val id:Int) {
+    NEW(101),
+    COOKING(102),
+    COMPLETED(103),
+    ERROR(0),
 }
 
 fun setStatus(status: Status){
